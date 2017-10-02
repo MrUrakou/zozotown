375be6d3226300726f77ffae736309e3aebf78fa
浦田　幸嗣 <btz0727@me.com>
2017-10-02 15:40:07 +0900

    Add carams to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001081444) do
+ActiveRecord::Schema.define(version: 20171002061527) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20171001081444) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "name"
+    t.string "kana_name"
+    t.integer "postal_code"
+    t.string "address1"
+    t.string "address2"
+    t.integer "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
