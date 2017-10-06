238a10533d2e14051a76c10154e9a489d2e43d35
浦田　幸嗣 <btz0727@me.com>
2017-10-06 16:48:46 +0900

    Edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  root "products#index"
-  #resources :products, only: [:index]
+  root "admin/products#index"
+  resources :users, only:[:edit, :update]
+  resources :products, only: [:index]
+
+  namespace :admin do
+    resources :products, only: [:index, :new, :create]
+  end
 end
