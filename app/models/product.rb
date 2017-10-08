2080c1cc5d6f2d2d5524873a7190c2f9bea9c1e7
浦田　幸嗣 <btz0727@me.com>
2017-10-08 18:21:16 +0900

    Add association cart and cart_product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_many :users, through: :product_users
+  has_many :carts
   validates :name, :category, :image, :price, :product_code, :count, :description, presence: true
 end
