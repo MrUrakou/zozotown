61ce5ec383510bb7082612809f7a80b473787fba
浦田　幸嗣 <btz0727@me.com>
2017-10-10 17:58:55 +0900

    Change orders action index to show

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products, only: [:index, :show, :new, :create]
-    resources :orders, only: [:index]
+    resources :orders, only: [:show]
   end
 end
