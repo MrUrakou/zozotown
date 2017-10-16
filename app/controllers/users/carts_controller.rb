a909ca08093d84c4fd6a1c637ce2b4b464512f2a
浦田　幸嗣 <btz0727@me.com>
2017-10-16 17:11:41 +0900

    Edit key name

--- app/controllers/users/carts_controller.rb
+++ app/controllers/users/carts_controller.rb
@@ -17,7 +17,7 @@ class Users::CartsController < ApplicationController
   end
 
   def purchase
-    Payjp.api_key = PAY_SECRET_KEY
+    Payjp.api_key = PAYJP_SECRET_KEY
     Payjp::Charge.create(currency: 'jpy', amount: 1000, card: params['payjp-token'])
     redirect_to root_path, notice: "支払いが完了しました"
   end
