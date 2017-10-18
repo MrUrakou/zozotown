dbb770dd5ce8fb94d458fc9df2186599a76ac508
浦田　幸嗣 <btz0727@me.com>
2017-10-18 17:34:40 +0900

    Edit product new and classic

--- app/controllers/users/products_controller.rb
+++ app/controllers/users/products_controller.rb
@@ -1,9 +1,11 @@
 class Users::ProductsController < ApplicationController
   def index
-    @products = Product.order("created_at DESC").page(params[:page]).per(6)
+    @new_products = Product.where(created_at: Time.mktime(2017, 10, 18).all_day).limit(6)
+    @classic_products = Product.where(created_at: Time.mktime(2017, 10, 17).all_day).page(params[:page]).per(6)
   end
 
   def show
     @product = Product.find(params[:id])
   end
 end
+User.where(created_at: 1.day.ago.all_day)
